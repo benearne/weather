3770e0f78484c4cb40f83a6ea757d29315f782fe
„benearne“ <b.arnold@posteo.de>
2025-06-14 18:22:27 +0200

    add publish stuff

--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,18 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
+    filename: 'bundle.js', // optional: 'bundle.[contenthash].js' wenn du willst
+    publicPath: '', // wichtig für GitHub Pages
     clean: true,
   },
-  mode: 'development',
+  mode: 'development', // für GitHub Pages kann auch 'production' sinnvoll sein
   devServer: {
     static: './dist',
     open: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/template.html',
+      template: './public/template.html', // erzeugt dist/index.html automatisch
     }),
   ],
   module: {
@@ -30,10 +31,5 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
     ],
-  },
-  output: {
-	path: path.resolve(__dirname, 'dist'),
-	filename: 'bundle.js',
-	publicPath: '',
   },
 };
